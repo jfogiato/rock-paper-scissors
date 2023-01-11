c07794cc25d61c1e7210c77dab870fba16d56c26
Joe Fogiato <joe.fogiato@gmail.com>
2023-01-10 19:53:38 -0500

    Feat: Full game functionality via console

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var currentGame// = new Game only instantiated after user clicks game;
-var user// = currentGame.player only instantiated after user clicks game;
-var computer// = currentGame.computer only instantiated after user clicks game;
-var fighters// = currentGame.getCleanBoard();
+var currentGame = new Game('hard'); // = new Game only instantiated after user clicks game;
+var user = currentGame.player; // = currentGame.player only instantiated after user clicks game;
+var computer = currentGame.computer; // = currentGame.computer only instantiated after user clicks game;
+var fighters = currentGame.getCleanBoard(); // = currentGame.getCleanBoard();
 
 var userSection = document.getElementById('userSection');
 var userWins = document.getElementById('userWins');
@@ -28,6 +28,10 @@ var hardFightersSection = document.getElementById('hardFighters');
 
 
 /*
+ - Should I chunk the two front page sections together to apply one listener and return the H4 of that listener?
+ - Should I do the same^^^ for the two fighters sections?
+
+
  - Clicking one of the game options (classic or hard) generates a new instance of Game and sets the type equal to the type that was clicked.
  - Based on Game.type, appropriate list of fighters is displayed on DOM (3 or 5)
  - On clicking one of the fighters, Game method is called to 
