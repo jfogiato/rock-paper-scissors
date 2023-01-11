0e948d661d217cedcf85c8b68ff59be2fa4d6831
Joe Fogiato <joe.fogiato@gmail.com>
2023-01-11 12:08:51 -0500

    Feat: add funtionality to hide/show appropriate pages based on game choice

--- main.js
+++ main.js
@@ -1,25 +1,45 @@
-var currentGame = new Game('hard'); // = new Game only instantiated after user clicks game;
+// Global Variables/Data Model 👇
+var currentGame = new Game(); // = new Game only instantiated after user clicks game;
 var user = currentGame.player; // = currentGame.player only instantiated after user clicks game;
 var computer = currentGame.computer; // = currentGame.computer only instantiated after user clicks game;
 var fighters = currentGame.getCleanBoard(); // = currentGame.getCleanBoard();
 
+// DOM Element Variables 👇
 var userSection = document.getElementById('userSection');
 var userWins = document.getElementById('userWins');
 var computerSection = document.getElementById('computerSection');
 var computerWins = document.getElementById('computerWins');
-var changeGameBtn = document.getElementById('changeGameButton');
+
+var choiceSection = document.getElementById('choiceSection');
 var classicSection = document.getElementById('classicSection');
 var hardSection = document.getElementById('hardSection');
+var fighterSection = document.getElementById('fighterSection');
 var classicFightersSection = document.getElementById('classicFighters');
 var hardFightersSection = document.getElementById('hardFighters');
 
+var changeGameBtn = document.getElementById('changeGameButton');
+
+// Event Listeners 👇
+choiceSection.addEventListener('click', () => {
+  generateVariables(event);
+});
 
 
+// Event Handlers/Functions 👇
+function generateVariables(event) {
+  var gameChoice = event.target.parentNode.id // classicSection or hardSection
+  toggleFighters(gameChoice);
+}
 
+function toggleFighters(gameChoice) {
+  choiceSection.classList.toggle('hidden');
+  fighterSection.classList.toggle('hidden');
+  chooseSection(gameChoice)
+}
 
-
-
-
+function chooseSection(choice) {
+  choice === 'classicSection' ? hardFighters.classList.add('hidden') : classicFighters.classList.add('hidden');
+}
 
 
 
@@ -28,10 +48,6 @@ var hardFightersSection = document.getElementById('hardFighters');
 
 
 /*
- - Should I chunk the two front page sections together to apply one listener and return the H4 of that listener?
- - Should I do the same^^^ for the two fighters sections?
-
-
  - Clicking one of the game options (classic or hard) generates a new instance of Game and sets the type equal to the type that was clicked.
  - Based on Game.type, appropriate list of fighters is displayed on DOM (3 or 5)
  - On clicking one of the fighters, Game method is called to 
